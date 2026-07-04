5a0a63e8d18515dabe6710d33f17b4a98ea3ecfb
sandeep <gangasandeep239@gmail.com>
2026-07-04 20:38:40 +0530

    cookie file added for transcription

--- LLM.py
+++ LLM.py
@@ -31,6 +31,7 @@ class YouTubeAIPipeline:
             # If you don’t care which language, this returns the “best” one
             transcript_li = YouTubeTranscriptApi()
             transcript_list=transcript_li.fetch(video_id,languages=['en'])
+            cookies=cookies.txt
 
             # Flatten it to plain text
             transcript = " ".join(chunk.text for chunk in transcript_list)
